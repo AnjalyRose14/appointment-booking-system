630cd25467e888b797c98c9851821ff6bc583c0d
Anjaly <anjalyrose14@gmail.com>
2025-11-06 17:27:50 +0530

    initial

--- src/main/java/com/unibooking/backend/user/service/UserService.java
+++ src/main/java/com/unibooking/backend/user/service/UserService.java
@@ -1,15 +1,31 @@
 package com.unibooking.backend.user.service;
 
 import com.unibooking.backend.Exception.UserAlreadyExistsException;
+import com.unibooking.backend.Exception.UserNotFoundException;
 import com.unibooking.backend.user.dto.*;
 import com.unibooking.backend.user.model.UserModel;
 
+import java.util.List;
 import java.util.Optional;
 public interface UserService {
 
     // Get logged-in user's profile
-    Optional<UserModel> getCurrentUserProfile(String email);
+    UserDTO getUserProfile(String email) throws UserNotFoundException;
+    //get all users
+    List<UserDTO> getAllUsers();
 
     // register new user
-    Boolean createUserProfile(UserDTO userDTO) throws UserAlreadyExistsException;
+    Boolean createUserProfile(RegisterDTO registerDTO) throws UserAlreadyExistsException;
+
+    // login user
+    Boolean loginUser(LoginDTO loginDTO);
+
+    // Update existing user profile
+    UserModel updateUserProfile(UpdateDTO updateDTO) throws UserNotFoundException;
+
+    //delete user
+    void deleteUser(Long id) throws UserNotFoundException;
+
+
+    Object convertToDTO(UserModel userModel);
 }
